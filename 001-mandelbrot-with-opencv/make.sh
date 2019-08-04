f63ea2b94e5da966e1aec633288c4daea07e9e36
Tao PR <pataoengineer@gmail.com>
2019-08-04 12:29:44 +0200

    001 add initial setup

---
+++ 001-mandelbrot-with-opencv/make.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+export OPENCV_DIR=/usr/local/Cellar/opencv/4.1.0_2/
+export OPENCV_INCLUDE_DIR=$OPENCV_DIR/include/opencv4/
+export OPENCV_LIB_DIR=$OPENCV_DIR/lib/
+
+cd build
+
+echo "================"
+echo "Preparing recipe"
+echo "================"
+cmake -DCMAKE_CXX_COMPILER=$(which g++) \
+      -DCMAKE_CXX_FLAGS="-std=c++11 -Wall -g -O1" -LAH ..
+
+echo "================"
+echo "Building..."
+echo "================"
+make
