1b38e78055ecee24d00ed3f730ad67e5dda941cd
Tao PR <pataoengineer@gmail.com>
2019-08-04 13:34:44 +0200

    add initial mandelbrot empty class

--- 001-mandelbrot-with-opencv/headers/Mandelbrot.hpp
+++ 001-mandelbrot-with-opencv/headers/Mandelbrot.hpp
@@ -6,7 +6,16 @@
 #include <opencv2/opencv.hpp>
 #include <opencv2/highgui/highgui.hpp>
 
+using namespace cv;
+using namespace std;
+
 class Mandelbrot
 {
+private:
+protected:
+public:
+  Mandelbrot(int nMaxIters=10);
+  ~Mandelbrot() {};
 
+  void render(Size& size);
 };
---
+++ 001-mandelbrot-with-opencv/src/Mandelbrot.cpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "Mandelbrot.hpp"
+
+Mandelbrot::Mandelbrot(int nMaxIters)
+{
+
+}
+
+void Mandelbrot::render(Size& size)
+{
+  
+}
