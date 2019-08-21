fb6e57cb0443ed273e914556c6154e17f4b8e1ce
starcolon <tao@starcolon.com>
2019-08-22 01:24:54 +0200

    002 wip

--- 002-PR-quadtree-haskell/prquadtree/src/PRQuad/Tree.hs
+++ 002-PR-quadtree-haskell/prquadtree/src/PRQuad/Tree.hs
@@ -1,28 +1,33 @@
 module PRQuad.Tree where
 
-type Coord = (Int,Int) -- x,y
-type Bound = (Int,Int) -- width,height
+type Coord = (Int,Int) deriving Show -- x,y
+type Bound = (Int,Int,Int,Int) deriving Show -- x0,y0,x1,y1
 
 -- Quadtree is represented by a composition of
 -- 4 quadrants containing data points.
-data QTree = EmptyTree
-  | Sole Coord
+data QTree = EmptyTree Bound
+  | Sole Bound Coord
   | QTree Bound QTree QTree QTree QTree -- q1 q2 q3 q4
 
 
--- Locate the best quadrant a coordinate of a Quadtree where it can lie on
+-- Locate the best quadrant of a Quadtree where a coordinate can lie on
 locateQuadrant :: Coord => QTree => Int
 ???
+
+-- Locate the best quadrant of a rectangular bound where a coordinate can lie on 
+locateQuadrantBound :: Coord => Bound => Int
+locateQuadrant (x,y) (x0,y0,x1,y1) = ???
 
 getQuadrant :: Int => QTree => QTree
 ???
 
+
 isEmpty :: QTree => Bool
-isEmpty EmptyTree => True
+isEmpty EmptyTree _ => True
 isEmpty _ => False
 
 isSole :: QTree => Bool
-isSole (Sole c) => True
+isSole (Sole _ _) => True
 isSole _ => False
 
 insertTo :: Coord => QTree => QTree
@@ -31,4 +36,6 @@ insertTo :: Coord => QTree => QTree
 count :: QTree => Int
 ???
 
+findClosest :: Coord => QTree => Maybe Coord
+???
 
