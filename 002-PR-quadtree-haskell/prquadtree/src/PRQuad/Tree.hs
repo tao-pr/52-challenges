274f103becc980a4933e2b4c9bb2e335b1a680d4
Tao PR <pataoengineer@gmail.com>
2019-08-24 23:36:26 +0200

    002 make it build

--- 002-PR-quadtree-haskell/prquadtree/src/PRQuad/Tree.hs
+++ 002-PR-quadtree-haskell/prquadtree/src/PRQuad/Tree.hs
@@ -1,7 +1,7 @@
 module PRQuad.Tree where
 
-type Coord = (Int,Int) deriving Show -- x,y
-type Bound = (Int,Int,Int,Int) deriving Show -- x0,y0,x1,y1
+type Coord = (Int,Int) -- x,y
+type Bound = (Int,Int,Int,Int) -- x0,y0,x1,y1
 
 -- Quadtree is represented by a composition of
 -- 4 quadrants containing data points.
@@ -11,13 +11,13 @@ data QTree = EmptyTree Bound
 
 
 -- Locate the best quadrant of a Quadtree where a coordinate can lie on
-locateQuadrant :: Coord => QTree => Int
-locateQuadrant (x,y) EmptyTree = 1
+locateQuadrant :: Coord -> QTree -> Int
+locateQuadrant (x,y) (EmptyTree b) = locateQuadrantBound (x,y) b
 locateQuadrant (x,y) (Sole b _) = locateQuadrantBound (x,y) b
-locateQuadrant (x,y) (QTree b q1 q2 q3 q4) = (x,y) b
+locateQuadrant (x,y) (QTree b q1 q2 q3 q4) = locateQuadrantBound(x,y) b
 
 -- Locate the best quadrant of a rectangular bound where a coordinate can lie on 
-locateQuadrantBound :: Coord => Bound => Int
+locateQuadrantBound :: Coord -> Bound -> Int
 locateQuadrantBound (x,y) (a,b,c,d) = 
   let{cx = quot (c-a) 2;
       cy = quot (d-b) 2}
@@ -26,24 +26,24 @@ locateQuadrantBound (x,y) (a,b,c,d) =
       else if x>=cx && y>=cy then 1
       else 3
 
-getQuadrant :: Int => QTree => QTree
-???
+getQuadrant :: Int -> QTree -> QTree
+getQuadrant _ _ = error "TAOTODO"
 
 
-isEmpty :: QTree => Bool
-isEmpty EmptyTree _ => True
-isEmpty _ => False
+isEmpty :: QTree -> Bool
+isEmpty (EmptyTree b) = True
+isEmpty _ = False
 
-isSole :: QTree => Bool
-isSole (Sole _ _) => True
-isSole _ => False
+isSole :: QTree -> Bool
+isSole (Sole _ _) = True
+isSole _ = False
 
-insertTo :: Coord => QTree => QTree
-???
+insertTo :: Coord -> QTree -> QTree
+insertTo _ _ = error "TAOTODO"
 
-count :: QTree => Int
-???
+count :: QTree -> Int
+count _ = error "TAOTODO"
 
-findClosest :: Coord => QTree => Maybe Coord
-???
+findClosest :: Coord -> QTree -> Maybe Coord
+findClosest _ _ = error "TAOTODO"
 
