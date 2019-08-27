9be658424f8934902f7b08b9ba48c4e6b87221b0
Tao PR <pataoengineer@gmail.com>
2019-08-28 00:43:02 +0200

    002 fix quadrant and coordinate system

--- 002-PR-quadtree-haskell/prquadtree/test/PRQuad/TreeSpec.hs
+++ 002-PR-quadtree-haskell/prquadtree/test/PRQuad/TreeSpec.hs
@@ -10,7 +10,7 @@ main :: IO ()
 main = hspec spec
 
 _b_ ::Bound 
-_b_ = (-5,-5,7,7)
+_b_ = (-5,7,7,-5)
 
 spec :: Spec
 spec = do
@@ -19,14 +19,14 @@ spec = do
 
     it "split quadrants" $ do
       let qs = splitQuadrant _b_
-        in qs `shouldBe` [(1,1,7,7), (-5,1,1,7), (-5,-5,1,1), (1,-5,7,1)]
+        in qs `shouldBe` [(1,1,7,-5),(-5,1,1,-5),(-5,7,1,1),(1,7,7,1)]
 
     it "locate quadrant" $ do
-      let{i1 = locateQuadrantBound (-4,1) _b_;
-          i2 = locateQuadrantBound (3,0) _b_;
-          i3 = locateQuadrantBound (1,1) _b_;
-          i4 = locateQuadrantBound (4,5) _b_}
-        in [i1, i2, i3, i4] `shouldBe` [3,4,1,1]
+      let{i1 = locateQuadrantBound (-4,4) _b_;
+          i2 = locateQuadrantBound (4,3) _b_;
+          i3 = locateQuadrantBound (-2,-4) _b_;
+          i4 = locateQuadrantBound (4,-4) _b_}
+        in [i1, i2, i3, i4] `shouldBe` [3,4,2,1]
  
   describe "Basic creation and insertion" $ do
 
