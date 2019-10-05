b946b4f5c6e08895ac6d49f54853ee22a3fef425
starcolon <tao@starcolon.com>
2019-10-05 23:31:00 +0200

    004 entry point for training

--- 004-skewed-data-classification/libdata/__main__.py
+++ 004-skewed-data-classification/libdata/__main__.py
@@ -9,17 +9,58 @@ import pandas as pd
 
 from libdata import wrangle, func, feature, pipeline
 
+def load_csv(filename):
+  city_temp_file = os.path.join(
+    os.environ['HOME'], 
+    'data',
+    'global-temperature',
+    filename)
+  print('Reading {}'.format(city_temp_file))
+  return pd.read_csv(city_temp_file)
+
+def get_clean_data(for_train: bool):
+  """
+  Load raw data from a csv file.
+  Polish features and discard unwanted columns.
+  """
+  print('Polishing features')
+  df = load_csv('GlobalLandTemperaturesByCity.csv')
+  df = wrangle.split_month_year(df)
+  df = df[['AverageTemperature','City','Country',
+           'Latitude','Longitude',
+           'year','month']]
+  df = df[df['AverageTemperature'].notnull()]
+
+  print('Filtering data')
+  if for_train:
+    df = df[(df['year']>=1900) & (df['year']<=2000)].sample(frac=0.1)
+  else:
+    df = df[(df['year']>=2001) & (df['year']<=2010)]
+
+  print('Raw data size for feature polish : {}'.format(len(df)))
+  features = feature.add_feature_prev_temp(df)
+  features = feature.add_feature_latlng(features)
+  features.drop(['City','Latitude','Longitude'], axis=1, inplace=True)
+  print('Feature size : {}'.format(len(features)))
+  return features
+
 def classification():
+  df = get_clean_data(for_train=False)
+  model = pickle.load(open('model.pkl', 'rb'))
+  print('Ready for classification ...')
   pass
 
 def training():
-  pass
+  df = get_clean_data(for_train=True)
+  print('Ready for training ...')
+  print('Training data size : {}'.format(len(df)))
+
 
 if __name__ == '__main__':
   print('Executing libdata ...')
   if os.path.isfile('model.pkl'):
-    print('Model already exists, doing classification')
+    print('Running classification with pre-trained model')
     classification()
   else:
-    print('Start training process')
+    print('Starting training process')
     training()
