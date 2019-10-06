87f7c3025466ea5dbaec50f36ae422754c0ed549
Tao PR <pataoengineer@gmail.com>
2019-10-06 23:43:18 +0200

    004 wip full train with cv

--- 004-skewed-data-classification/libdata/pipeline.py
+++ 004-skewed-data-classification/libdata/pipeline.py
@@ -1,16 +1,12 @@
+import numpy as np
+
 from sklearn.pipeline import *
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import LabelEncoder
 from sklearn.linear_model import LinearRegression
 
-clf = make_pipeline(
-  memory=None,
-  steps=[
-    ('CountryEncoder', CountryEncoder(input_col='Country', output_col='cnt')),
-    ('FeatureSelector', ColumnsSelector()),
-    ('Regressor', Regressor(blueprint=LinearRegression(
-      normalize=True, n_jobs=4)))
-    ])
+def load_model(path):
+  return pickle.load(open(path, 'rb'))
 
 class ColumnsSelector(BaseEstimator, TransformerMixin):
   """
@@ -46,14 +42,46 @@ class CountryEncoder(BaseEstimator, TransformerMixin):
 
 class Regressor(BaseEstimator, TransformerMixin):
 
-  def __init__(self, blueprint: BaseEstimator):
+  def __init__(self, blueprint: BaseEstimator, output_col: str, split_ratio: float):
     self.model = blueprint
+    self.output_col = output_col
+    self.split_ratio = split_ratio
 
   def fit(self, Z, y=None):
     (X,y) = Z
-    self.model.fit(X, y)
+
+    # Split the dataset into training and testing
+    cutoff = int(self.split_ratio * len(X))
+    X_train = X[:cutoff]
+    X_test = X[cutoff:]
+    y_train = y[:cutoff]
+    y_test = y[cutoff:]
+
+    print('Training set size : {}'.format(len(X_train)))
+    print('Test set size.    : {}'.format(len(X_test)))
+
+    self.model.fit(X_train, y_train)
+
+    print('Running cross validation')
+    y_pred = self.model.predict(X_test)
+
+    # Measure mean square error
+    rmse = np.sqrt(np.sum(np.square(y_pred) - np.square(y_test)))/float(len(y_pred))
+    print('mean square error : {}'.format(rmse))
+
     return self
 
-  def transform(self, X, y=None):
-    return self.model.transform(X)
+  def transform(self, Z, y=None):
+    (X,y) = Z
+    X[self.output_col] = self.model.predict(X)
+    return X
 
+
+clf = make_pipeline(
+    CountryEncoder(input_col='Country', output_col='cnt'),
+    ColumnsSelector(),
+    Regressor(
+      blueprint=LinearRegression(normalize=True, n_jobs=4),
+      output_col='pred_temp_change',
+      split_ratio=0.85)
+    )
