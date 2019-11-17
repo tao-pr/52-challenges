24bff31ba1a57457643b61bf9876177c33bfd6e3
starcolon <tao@starcolon.com>
2019-11-17 17:39:40 +0100

    005 wip design calculation

--- 005-fourier-transform-haskell/fft/src/Fourier/FFT.hs
+++ 005-fourier-transform-haskell/fft/src/Fourier/FFT.hs
@@ -6,17 +6,18 @@ import Data.Matrix(matrix,fromList)
 
 -- Cooley & Tukey Fast Fourier transformation of a sequence
 fft :: [Double] -> [Complex Double]
-fft fs = error "not implemented"
+fft xs = dft xs
 
 -- Typical Discrete Fourier transformation of a sequence
 dft :: [Double] -> [Complex Double]
 dft [] = []
-dft fs = error "not implemented"
+dft xs = do { k <- [0..length xs - 1]
+  ; let re = sumCoef k xs cos
+  ; let im = sumCoef k xs sin
+  ; return (re :+ im)}
 
--- Compute DFT coefficient at index
-dftAt :: Int -> [Double] -> [Complex Double]
-dftAt _ [] = [0.0:+0.0]
-dftAt i fs = if length fs <= i then [0.0:+0.0] else 
-  [1.0:+0.0]
+-- Calculate the sum of coefficience at [k]
+sumCoef :: Int -> [Double] -> (Double -> Double) -> Double
+sumCoef _ _ _ = error "not implemented"
 
 
