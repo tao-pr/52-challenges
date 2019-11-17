0194c40290f50767957eedf4dbba1a9f23bdf210
starcolon <tao@starcolon.com>
2019-11-17 18:51:08 +0100

    005 implement dft

--- 005-fourier-transform-haskell/fft/src/Fourier/FFT.hs
+++ 005-fourier-transform-haskell/fft/src/Fourier/FFT.hs
@@ -12,12 +12,16 @@ fft xs = dft xs
 dft :: [Double] -> [Complex Double]
 dft [] = []
 dft xs = do { k <- [0..length xs - 1]
-  ; let re = sumCoef k xs cos
-  ; let im = sumCoef k xs sin
+  ; let re = xk k xs cos
+  ; let im = -1 * (xk k xs sin)
   ; return (re :+ im)}
 
 -- Calculate the sum of coefficience at [k]
-sumCoef :: Int -> [Double] -> (Double -> Double) -> Double
-sumCoef _ _ _ = error "not implemented"
+-- Formula: xk = sum x[n] f(2pi * k * n/N)
+xk :: Int -> [Double] -> (Double -> Double) -> Double
+xk k xs f = let ys = do { (i,x) <- zip [0..] xs
+  ; let n = fromIntegral $ length xs
+  ; return (x * f( 2.0 * pi * (fromIntegral k) * (fromIntegral i) / n)) }
+    in sum ys
 
 
