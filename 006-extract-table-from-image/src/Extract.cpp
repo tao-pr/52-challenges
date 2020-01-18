7db7ea1d975304b5eac5fcb7232a1b279f888ac7
starcolon <tao@starcolon.com>
2020-01-18 16:21:19 +0100

    006 read image

--- 006-extract-table-from-image/src/Extract.cpp
+++ 006-extract-table-from-image/src/Extract.cpp
@@ -4,13 +4,20 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+#include <opencv2/opencv.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
 #include "Extractor.hpp"
 
 int main(int argc, char** argv)
 {
+  // Read in the image
+  Mat im = imread("./data/20200117134920228.jpg", IMREAD_GRAYSCALE);
+  imshow("original", im);
+  moveWindow("original", 0, 0);
 
 
-
+  waitKey(0);
 }
 
 #endif
