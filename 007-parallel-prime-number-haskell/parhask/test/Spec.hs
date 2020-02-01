63b6bce55a01f96e4c42d0538b07efbd7182453d
Tao PR <pataoengineer@gmail.com>
2020-02-01 10:26:19 +0100

    007 find multiples with test

--- 007-parallel-prime-number-haskell/parhask/test/Spec.hs
+++ 007-parallel-prime-number-haskell/parhask/test/Spec.hs
@@ -1,19 +1,1 @@
 {-# OPTIONS_GHC -F -pgmF hspec-discover #-}
-
-module Lib.Spec (main,spec) where
-
-import Test.Hspec
-import Test.QuickCheck()
-import Lib
-
-main :: IO ()
-main = hspec spec
-
-spec :: Spec
-spec = do
-
-  describe "Fundamentals" $ do
-
-    it "finds multiples of a number" $ do
-
-      error "no implementation"
