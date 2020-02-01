3383973dd87b55701c78817e1a9e445e5d6303e5
starcolon <tao@starcolon.com>
2020-02-01 10:10:10 +0100

    multiples of

--- 007-parallel-prime-number-haskell/parhask/test/Spec.hs
+++ 007-parallel-prime-number-haskell/parhask/test/Spec.hs
@@ -1,2 +1,19 @@
+{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
+
+module Lib.Spec (main,spec) where
+
+import Test.Hspec
+import Test.QuickCheck()
+import Lib
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec spec
+
+spec :: Spec
+spec = do
+
+  describe "Fundamentals" $ do
+
+    it "finds multiples of a number" $ do
+
+      error "no implementation"
