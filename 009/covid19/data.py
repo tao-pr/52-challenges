df2db3ca7de3451fa967124df261237d86b2b51a
Tao PR <pataoengineer@gmail.com>
2020-03-22 13:40:53 +0100

    009 : added load daily data

--- 009/covid19/data.py
+++ 009/covid19/data.py
@@ -1,7 +1,9 @@
+import sys
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+from termcolor import colored
 from os import listdir
 from os.path import isfile, join
 
@@ -12,5 +14,45 @@ def load_daily_cases(d):
   """
 
   # List all daily case files
-  daily = [f for f in listdir(d) if isfile(join(d, f)) and f.endswith(".csv")]
-  print(daily)
+  q = join(d, "csse_covid_19_data/csse_covid_19_daily_reports")
+  csv_list = [f for f in listdir(q) if isfile(join(q, f)) and f.endswith(".csv")]
+  daily = []
+  for tag in csv_list:
+    print(colored("Reading : ", "cyan"), tag)
+    with open(join(q,tag), "r") as f:
+      # CSV is poorly formatted, there are preceding commas at the beginning
+      # of many lines so let's read them manually
+      header = []
+      for l in f.readlines():
+        tokens = l.split(",")
+
+        # Register header if hasn't
+        if len(header)==0:
+          header = [a for a in tokens]
+          continue
+
+        # Empty preceding tokens (null province)
+        # will be replaced with country name (subsequent token)
+        if len(tokens[0])==0:
+          tokens[0] = tokens[1]
+
+        kv = {a: b for a,b in zip(header,tokens)}
+
+        # Also add "date" as another column
+        kv["date"] = tag.split(".")[0] 
+        daily.append(kv)
+
+  daily = pd.DataFrame(daily)
+  print(daily[:5])
+  print(colored("Daily records read : ", "cyan"), len(daily), " rows")
+  return daily
+
+
+if __name__ == '__main__':
+  """
+  Usage: 
+    python3 -m covid19.data {PATH_TO_COVID19_GIT_REPOPATH}
+  """
+  path = sys.argv[-1]
+  print(colored("Loading daily cases from : ","cyan"), path)
+  daily = load_daily_cases(path)
