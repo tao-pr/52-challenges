151ebee45beb4763dc9ed1f8a3ec4d6aa64ec88a
starcolon <tao@starcolon.com>
2020-04-01 23:09:40 +0200

    010 plotted pickle v joblib

--- 010-profile-python-memory-access/memory_profile/serialise.py
+++ 010-profile-python-memory-access/memory_profile/serialise.py
@@ -1,6 +1,6 @@
 import sys
-import pickle
 import joblib
+import pickle
 import numpy as np
 
 from .data import Deep
@@ -9,29 +9,39 @@ if __name__ == '__main__':
   arg1 = sys.argv[-2]
   arg2 = sys.argv[-1]
 
-  if arg1 not in ["PICKLE","JOBLIB"]:
+  if arg1 not in ["PICKLE","JOBLIB","JOBLIB-COMPRESS"]:
     raise ValueError("Unknown run mode")
 
   if arg1 == "PICKLE":
     if arg2 == "WRITE":
       blob = Deep()
       print("Serialising with pickle")
-      with open("pickle", "w") as f:
+      with open("pickle", "wb") as f:
         pickle.dump(blob, f)
     else:
       print("Deserialising with pickle")
-      with open("pickle", "r") as f:
+      with open("pickle", "rb") as f:
         blob = pickle.load(f)
 
   elif arg1 == "JOBLIB":
     if arg2 == "WRITE":
       print("Serialising with joblib")
       blob = Deep()
-      print("Serialising with joblib")
-      with open("joblib", "w") as f:
+      with open("joblib", "wb") as f:
         joblib.dump(blob, f)
     else:
       print("Deserialising with joblib")
-      with open("joblib", "r") as f:
+      with open("joblib", "rb") as f:
+        blob = joblib.load(f)
+
+  elif arg1 == "JOBLIB-COMPRESS":
+    if arg2 == "WRITE":
+      print("Serialising with joblib compression")
+      blob = Deep()
+      with open("joblib-compressed", "wb") as f:
+        joblib.dump(blob, f, compress=1)
+    else:
+      print("Deserialising with joblib compression")
+      with open("joblib-compressed", "rb") as f:
         blob = joblib.load(f)
 
