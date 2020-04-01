a651d6f4dccdd7451e4bf0ce093f3ab30a83b51b
starcolon <tao@starcolon.com>
2020-04-01 22:56:53 +0200

    010 added initial serialise module

---
+++ 010-profile-python-memory-access/memory_profile/data.py
@@ -0,0 +1,16 @@
+from pickle import 
+from .data import M
+
+class M:
+  def __init__(self):
+    self.data = [np.arange(1,5000) for i in range(1000)]
+
+  def update(self):
+    self.data = [v*-1 for v in self.data]
+    return self
+
+
+class Deep:
+  def __init__(self):
+    self.a = [np.arange(1,5000) for i in range(1000)]
+    self.d = {"key": M()}
