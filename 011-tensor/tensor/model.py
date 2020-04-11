5820bdfdae6adee9e103606a88cd5400a57fd7d8
starcolon <tao@starcolon.com>
2020-04-12 00:24:01 +0200

    011 reduced complexity of model

--- 011-tensor/tensor/model.py
+++ 011-tensor/tensor/model.py
@@ -9,14 +9,20 @@ def build(w: int):
     w (int): Width of input image
   """
   model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(filters=5, kernel_size=(3,3), input_shape=(w,w,1)),
+    # 512x512
+    tf.keras.layers.Conv2D(filters=8, kernel_size=(3,3), input_shape=(w,w,1)),
     tf.keras.layers.MaxPool2D(pool_size=(2,2)),
-    tf.keras.layers.Conv2D(filters=5, kernel_size=(3,3), activation='relu'),
+    # 256x256
+    tf.keras.layers.Conv2D(filters=4, kernel_size=(3,3), activation='relu'),
+    tf.keras.layers.MaxPool2D(pool_size=(4,4)),
     tf.keras.layers.SpatialDropout2D(rate=0.1),
+    # 64x64
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(1024, activation='relu'),
     tf.keras.layers.Dense(2, activation='relu')
   ])
   model.summary()
   return model
+
+if __name__ == '__main__':
+  m = build(512)
 
