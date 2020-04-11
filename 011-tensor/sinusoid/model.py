e7969b0e58f49f4c28dc9b0f808a2f29521a7607
starcolon <tao@starcolon.com>
2020-04-11 13:00:20 +0200

    011 added initial code structure

---
+++ 011-tensor/sinusoid/model.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+import numpy as np
+import joblib
+
+def build():
+  """
+  Build the model for training
+  """
+  pass
+
+def load(path):
+  pass
+
+def save(model,path):
+  pass
