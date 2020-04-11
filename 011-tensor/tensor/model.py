8434844daac5354b6dc4ed3ec4d409ee500d26cc
starcolon <tao@starcolon.com>
2020-04-11 23:52:02 +0200

    011 added initial model

--- 011-tensor/tensor/model.py
+++ 011-tensor/tensor/model.py
@@ -2,14 +2,14 @@ import tensorflow as tf
 import numpy as np
 import joblib
 
-def build():
+def build(imsize: int):
   """
   Build the model for training
   """
-  pass
+  model = tf.keras.models.Sequential([
+    # Normalise each image independently 
+    tf.keras.layers.LayerNormalization(epsilon=0.005, scale=False),
+    tf.keras.layers.Conv2D(filters=5, kernel_size=(3,3)),
+  ])
+  return model
 
-def load(path):
-  pass
-
-def save(model,path):
-  pass
