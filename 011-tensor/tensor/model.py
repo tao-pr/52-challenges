c6b43b91ebeb0a6e7f227bd912ad3d80f6dd8b6e
starcolon <tao@starcolon.com>
2020-04-12 14:55:43 +0200

    011 fixed df gen

--- 011-tensor/tensor/model.py
+++ 011-tensor/tensor/model.py
@@ -9,14 +9,14 @@ def build(w: int):
     w (int): Width of input image
   """
   model = tf.keras.models.Sequential([
-    # 512x512
+    # 256x256
     tf.keras.layers.Conv2D(filters=8, kernel_size=(3,3), input_shape=(w,w,1)),
     tf.keras.layers.MaxPool2D(pool_size=(2,2)),
-    # 256x256
+    # 127x127
     tf.keras.layers.Conv2D(filters=4, kernel_size=(3,3), activation='relu'),
     tf.keras.layers.MaxPool2D(pool_size=(4,4)),
     tf.keras.layers.SpatialDropout2D(rate=0.1),
-    # 64x64
+    # 31x31
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(2, activation='relu')
   ])
@@ -25,5 +25,5 @@ def build(w: int):
 
 if __name__ == '__main__':
   # Test building and inspect the model structure
-  m = build(512)
+  m = build(256)
 
