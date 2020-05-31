282a8a8b302849a353e5a8648180064a157dd512
starcolon <tao@starcolon.com>
2020-06-01 01:13:41 +0200

    013 init data

--- 013-flatbuffers-serialisation/src/Main.cpp
+++ 013-flatbuffers-serialisation/src/Main.cpp
@@ -6,21 +6,33 @@
 
 #include "flatbuffers/flatbuffers.h"
 
-#include "data.hpp"
-#include "functions.hpp"
+#include "data_generated.h"
 
 using namespace std;
+using namespace Data;
 
 int main(int argc, char** argv)
 {
   cout << "Starting ..." << endl;
-  auto products = generateProducts();
+  flatbuffers::FlatBufferBuilder b(5*1024); // buffer size of 5 MB
+  
+  // Initialise data for serialisation
+  vector<flatbuffers::Offset<flatbuffers::String>> vec1 = {
+    b.CreateString("Frankfurt"), b.CreateString("Köln"), b.CreateString("Amsterdam")
+  };
+  auto stops1 = b.CreateVector(vec1);
+  auto route1 = CreateRoute(
+    b, 
+    stops1, 
+    Status_Operating, 
+    b.CreateString("DB"),
+    6);
 
-  assert(products.size() == 25);
 
-  // TAOTODO
+  // Serialise data into file
 
 
+  // Deserialise from file
 
 
   cout << "Ending ..." << endl;
