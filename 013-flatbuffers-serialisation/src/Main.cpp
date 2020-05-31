39a3feecc1788aa47e83605476f30ce3eb4e82d3
starcolon <tao@starcolon.com>
2020-06-01 01:16:47 +0200

    013 init more data

--- 013-flatbuffers-serialisation/src/Main.cpp
+++ 013-flatbuffers-serialisation/src/Main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char** argv)
   
   // Initialise data for serialisation
   vector<flatbuffers::Offset<flatbuffers::String>> vec1 = {
-    b.CreateString("Frankfurt"), b.CreateString("Köln"), b.CreateString("Amsterdam")
+    b.CreateString("Frankfurt"), b.CreateString("Köln"), b.CreateString("Düsseldorf"), b.CreateString("Amsterdam")
   };
   auto stops1 = b.CreateVector(vec1);
   auto route1 = CreateRoute(
@@ -28,6 +28,27 @@ int main(int argc, char** argv)
     b.CreateString("DB"),
     6);
 
+  vector<flatbuffers::Offset<flatbuffers::String>> vec2 = {
+    b.CreateString("Frankfurt"), b.CreateString("Ingolstadt"), b.CreateString("München")
+  };
+  auto stops2 = b.CreateVector(vec2);
+  auto route2 = CreateRoute(
+    b, 
+    stops2, 
+    Status_Operating, 
+    b.CreateString("DB"),
+    5);
+
+  vector<flatbuffers::Offset<flatbuffers::String>> vec3 = {
+    b.CreateString("Amsterdam"), b.CreateString("Rotterdam")
+  };
+  auto stops3 = b.CreateVector(vec3);
+  auto route3 = CreateRoute(
+    b, 
+    stops3, 
+    Status_Operating, 
+    b.CreateString("NS"),
+    40);
 
   // Serialise data into file
 
