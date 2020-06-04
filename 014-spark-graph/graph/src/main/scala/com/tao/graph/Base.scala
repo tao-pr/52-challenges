decc5b012eafd8e55f54e7d9a0d10bac01919c44
Tao PR <pataoengineer@gmail.com>
2020-06-05 00:11:18 +0200

    014 wip generate graph

--- 014-spark-graph/graph/src/main/scala/com/tao/graph/Base.scala
+++ 014-spark-graph/graph/src/main/scala/com/tao/graph/Base.scala
@@ -6,5 +6,6 @@ import org.apache.spark.graphx.{Graph, VertexRDD, EdgeRDD}
 // Graph representation
 
 class Node
+case object UndefinedNode extends Node
 case class City(name: String, size: Long) extends Node
 case class Station(name: String) extends Node
