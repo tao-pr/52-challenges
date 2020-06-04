6ecbc348082cb1927fe8639cf0314148e449a8b5
starcolon <tao@starcolon.com>
2020-06-04 22:05:49 +0200

    014 update gitignore

--- 014-spark-graph/graph/src/main/scala/com/tao/graph/Base.scala
+++ 014-spark-graph/graph/src/main/scala/com/tao/graph/Base.scala
@@ -1,14 +1,10 @@
 package com.tao.graph
 
 import org.apache.spark.graphx.Graph
+import org.apache.spark.graphx.{Graph, VertexRDD, EdgeRDD}
 
 // Graph representation
 
-object MetroGraph {
-
-  case class Station(name: String, cityName: String)
-
-  type Metro = Graph[Station, Double]
-
-}
-
+class Node
+case class City(name: String, size: Long) extends Node
+case class Station(name: String) extends Node
