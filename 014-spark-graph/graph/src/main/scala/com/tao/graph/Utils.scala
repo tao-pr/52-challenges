d409d00ed43c799615eca5f35f77a3c1f9d0da52
starcolon <tao@starcolon.com>
2020-06-05 22:39:45 +0200

    014 fix graph types

--- 014-spark-graph/graph/src/main/scala/com/tao/graph/Utils.scala
+++ 014-spark-graph/graph/src/main/scala/com/tao/graph/Utils.scala
@@ -5,15 +5,17 @@ import org.apache.spark.rdd.RDD
 import org.apache.spark.graphx.Graph
 import org.apache.spark.graphx.{Graph, VertexRDD, Edge}
 import org.apache.spark.graphx.VertexId
+import scala.collection.immutable.IndexedSeq
 
 object Util {
 
   // NOTE: RDD[(VertexId, VertexId)] is equivalent to Graph[VD, VertexId]
   def generateEdges(numEdges: Int, numNodes: Int)
-  (implicit sc: SparkContext): Graph[String, VertexId] = {
+  (implicit sc: SparkContext): Graph[String, Long] = {
     val generateEdgeTuple = (index: Int, numNodes: Int) => {
-      val Vector(a,b) = (0 to 1).map(_ => scala.math.abs(scala.util.Random.nextLong % numNodes.toLong))
-      Edge(a, b)
+      val Vector(a,b): IndexedSeq[Long] = (0 to 1).map(_ => scala.math.abs(scala.util.Random.nextLong % numNodes.toLong))
+      val attr = 0L
+      new Edge(a, b, attr)
     }
     val vertices = sc.parallelize((1 to numNodes).map{i => (i.toLong, s"City-$i")})
     val edges    = sc.parallelize((1 to numEdges).map{i => generateEdgeTuple(i, numNodes)})
