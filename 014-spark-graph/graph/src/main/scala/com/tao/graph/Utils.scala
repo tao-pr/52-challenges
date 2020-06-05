748975799b73af2a93366c3d3e2db55fd38d2b75
starcolon <tao@starcolon.com>
2020-06-06 00:50:13 +0200

    014 generate graph with edge type

--- 014-spark-graph/graph/src/main/scala/com/tao/graph/Utils.scala
+++ 014-spark-graph/graph/src/main/scala/com/tao/graph/Utils.scala
@@ -1,5 +1,7 @@
 package com.tao.graph
 
+import scala.reflect._
+
 import org.apache.spark.SparkContext
 import org.apache.spark.rdd.RDD
 import org.apache.spark.graphx.Graph
@@ -10,12 +12,23 @@ import scala.collection.immutable.IndexedSeq
 object Util {
 
   // NOTE: RDD[(VertexId, VertexId)] is equivalent to Graph[VD, VertexId]
-  def generateEdges(numEdges: Int, numNodes: Int)
+  def generateGraph(numEdges: Int, numNodes: Int)
   (implicit sc: SparkContext): Graph[String, Long] = {
     val generateEdgeTuple = (index: Int, numNodes: Int) => {
       val Vector(a,b): IndexedSeq[Long] = (0 to 1).map(_ => scala.math.abs(scala.util.Random.nextLong % numNodes.toLong))
       val attr = 0L
       new Edge(a, b, attr)
+    }
+    val vertices = sc.parallelize((1 to numNodes).map{i => (i.toLong, s"City-$i")})
+    val edges    = sc.parallelize((1 to numEdges).map{i => generateEdgeTuple(i, numNodes)})
+    Graph(vertices, edges)
+  }
+
+  def generateGraphWithEdgeAttr[A: ClassTag](numEdges: Int, numNodes: Int, edgeRandomiser: () => A)
+  (implicit sc: SparkContext): Graph[String, A] = {
+    val generateEdgeTuple = (index: Int, numNodes: Int) => {
+      val Vector(a,b): IndexedSeq[Long] = (0 to 1).map(_ => scala.math.abs(scala.util.Random.nextLong % numNodes.toLong))
+      new Edge(a, b, edgeRandomiser())
     }
     val vertices = sc.parallelize((1 to numNodes).map{i => (i.toLong, s"City-$i")})
     val edges    = sc.parallelize((1 to numEdges).map{i => generateEdgeTuple(i, numNodes)})
