cf169ddc01a9933b43646527e215c60019832b94
Tao PR <pataoengineer@gmail.com>
2020-06-09 00:56:53 +0200

    015 just update readme

--- 015-haskell-lens/hlens/README.md
+++ 015-haskell-lens/hlens/README.md
@@ -1,1 +1,20 @@
 # hlens
+
+Trying the famous Haskell Lens
+
+## Prerequisites
+
+- stack
+
+## Compile & Run
+
+Just do
+
+```bash
+$ stack build
+$ stack run
+```
+
+## Licence
+
+BSD
--- 015-haskell-lens/hlens/src/Recipe/HLens.hs
+++ 015-haskell-lens/hlens/src/Recipe/HLens.hs
@@ -4,5 +4,15 @@ module Recipe.HLens where
 import qualified Control.Lens
 import           Control.Lens.Operators
 
+-- Typeclasses
+
 class Flippable a where
   flip :: a -> a
+
+class Mergable a where
+  merge :: a -> a -> a
+
+
+-- Instances
+
+instance Mergable
