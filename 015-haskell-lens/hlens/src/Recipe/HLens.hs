411dae59b37944ce2c33e43e697847144817c3e1
Tao PR <pataoengineer@gmail.com>
2020-06-09 01:04:55 +0200

    015 just data

--- 015-haskell-lens/hlens/src/Recipe/HLens.hs
+++ 015-haskell-lens/hlens/src/Recipe/HLens.hs
@@ -4,15 +4,13 @@ module Recipe.HLens where
 import qualified Control.Lens
 import           Control.Lens.Operators
 
+-- Data
+
+data P a b = P a b | Q a | R [b]
+
 -- Typeclasses
 
 class Flippable a where
   flip :: a -> a
 
-class Mergable a where
-  merge :: a -> a -> a
-
-
 -- Instances
-
-instance Mergable
