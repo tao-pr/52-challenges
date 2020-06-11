c18d4896f9e163958b2562b69be41f78f3ae8987
Tao PR <pataoengineer@gmail.com>
2020-06-11 23:32:43 +0200

    015 make prism

--- 015-haskell-lens/hlens/src/Recipe/HLens.hs
+++ 015-haskell-lens/hlens/src/Recipe/HLens.hs
@@ -1,18 +1,27 @@
-{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE MultiParamTypeClasses #-}  -- For multiple-typed classes
+{-# LANGUAGE GADTs #-}                  -- For type-constraint typeclasses
+{-# LANGUAGE TemplateHaskell #-}        -- For Prism & Lens
+
 module Recipe.HLens where
 
-import qualified Control.Lens
-import           Control.Lens.Operators
+import Control.Lens
+import Control.Lens.TH 
 
 -- Data
 
-data Coord a = 
-  Coord  { x :: a, y :: a } | NoWhere |
-  ECoord { x :: a, y :: a, tlx :: a, tly :: a }  -- with tolerance of measurements
+data Coord a = Coord { _x :: a, _y :: a } deriving (Eq, Ord, Show)
+makeLenses ''Coord
+
+data Coord3D a = Coord3D { _xx :: a, _yy :: a, _zz :: a } deriving (Eq, Ord, Show)
+makeLenses ''Coord3D
+
+data Where a = AtCoord (Coord a) | AtCoord3D (Coord3D a) | NW
+  deriving (Eq, Ord, Show)
+makePrisms ''Where
+
+-- Oprs
+
+dist :: Coord a -> Coord a -> a
+dist p q = error "not implemented"
 
 
--- Typeclasses
-
-class Vec a where
-  i :: Ord b => a -> Maybe b
-  j :: Ord b => a -> Maybe b
