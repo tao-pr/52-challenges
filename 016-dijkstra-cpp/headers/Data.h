1f16402593a698571582bd676bc12712eeca1b48
starcolon <tao@starcolon.com>
2020-06-13 15:58:20 +0200

    016 wip input graph

--- 016-dijkstra-cpp/headers/Data.h
+++ 016-dijkstra-cpp/headers/Data.h
@@ -2,14 +2,34 @@
 
 #include <vector>
 #include <map>
+#include <set>
 #include <string>
 #include <iostream>
+#include <fmt/format.h>
 
 using namespace std;
 
 struct Graph {
   map<int, map<int, float>> edges; // Directional
-  map<int, string> nodes;
+  set<int> nodes;
+
+  void add(int v1, int v2, float w){
+    nodes.insert(v1);
+    nodes.insert(v2);
+    if (edges.find(v1) != edges.end()){
+      if (edges[v1].find(v2) != edges[v1].end()){
+        // TAOTODO
+      }
+      else {
+
+      }
+    }
+    else {
+      map<int, float> newv2;
+      newv2.insert(make_pair(v2, w));
+      edges.insert(make_pair(v1, newv2));
+    }
+  }
 };
 
 ostream &operator<<(ostream &os, Graph const &g){
@@ -20,8 +40,13 @@ ostream &operator<<(ostream &os, Graph const &g){
   for (auto const &[k,v] : g.edges){
     for (auto const &[kk,vv] : v){
       ++n;
-      cout << "... edge #" << n << " : " << k << " -> " 
-        << "WEIGHT" << " -> " << kk << endl;
+
+      fmt::print("... edge #{n} : {v1} -> {w:.1f} -> {v2} \n",
+        fmt::arg("n", n),
+        fmt::arg("v1", k),
+        fmt::arg("w", vv),
+        fmt::arg("v2", kk)
+      );
     }
   }
 };
--- 016-dijkstra-cpp/src/Main.cpp
+++ 016-dijkstra-cpp/src/Main.cpp
@@ -1,5 +1,7 @@
 #include <stdlib.h>
 #include <iostream>
+#include <sstream>
+#include <iterator>
 
 #include "Data.h"
 
@@ -9,7 +11,39 @@ int main(int argc, char** argv){
 
   cout << "Dijkstra ..." << endl;
 
-
+  // Inputs
+  auto g = Graph();
+  string input = "";
+  while (input != "n"){
+    cout << endl << "Input edge (v1,v2,w): ";
+    getline(cin, input);
+    if (input != "n"){
+      stringstream ss(input);
+      vector<string> chunks;
+      char part = 0;
+      int v1, v2;
+      double w;
+      string substr;
+      while (getline(ss, substr, ',')){
+        if (part==0){
+          // v1
+          v1 = stoi(substr);
+        }
+        else if (part==1){
+          // v2
+          v2 = stoi(substr);
+        }
+        else {
+          // weight
+          w = stof(substr);
+        }
+        ++part;
+      }
+      g.add(v1, v2, w);
+    }
+  }
+  cout << "Input complete " << endl;
+  cout << g << endl;
 }
 
 
