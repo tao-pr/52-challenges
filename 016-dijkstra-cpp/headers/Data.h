4102b056c6acf7822fd4d64fdd5f5409af5810c5
starcolon <tao@starcolon.com>
2020-06-14 00:44:42 +0200

    016 first attempt on dijkstra code

--- 016-dijkstra-cpp/headers/Data.h
+++ 016-dijkstra-cpp/headers/Data.h
@@ -1,17 +1,25 @@
 #pragma once
 
+#include <limits>
 #include <vector>
 #include <map>
 #include <set>
+#include <queue>
+#include <tuple>
 #include <string>
 #include <iostream>
 #include <fmt/format.h>
 
 using namespace std;
 
+#define NODE tuple<int,double>
+
 struct Path {
   vector<int> steps;
   float sumDistance;
+  Path(){
+    sumDistance = 0;
+  }
 };
 
 struct Graph {
@@ -36,8 +44,70 @@ struct Graph {
     }
   }
 
+  float findConnection(int v1, int v2){
+    if (v1==v2)
+      return 0;
+    else if (edges.find(v1)==edges.end())
+      return numeric_limits<float>::max();
+    else if (edges[v1].find(v2)==edges[v1].end())
+      return numeric_limits<float>::max();
+    else
+      return edges[v1][v2];
+  }
+
   Path& findShortest(int v1, int v2){
-    // TAOTODO
+    auto comp = [](NODE n1, NODE n2){ return get<1>(n1) > get<1>(n2); };
+    priority_queue<NODE, vector<NODE>, decltype(comp)> q(comp);
+
+    // Init Q & distance registry
+    map<int, float> distances;
+    for (const auto &v : nodes){
+      auto d = findConnection(v1,v);
+      q.push(make_pair(v, d));
+      distances[v] = d;
+    }
+
+    // Init steps
+    map<int, int> prev;
+
+    // Evaluate each node
+    while (!q.empty()){
+      auto tu = q.top(); q.pop();
+      int v = get<0>(tu);
+      float d = get<1>(tu);
+      
+
+      // Skip if the distance is not up-to-date
+      if (d != distances[v])
+        continue;
+
+      // Evaluate neighbours of v
+      if (edges.find(v) != edges.end()){
+        for (const auto& nextVW : edges[v]){
+          auto next = get<0>(nextVW);
+          float w = findConnection(v, next) + d;
+          if (distances.find(next)==distances.end() || w<distances[next]){
+            distances[next] = w;
+            prev[next] = v;
+            q.push(make_pair(next, w));
+          }
+        }
+      }
+    }
+
+    // Reconstruct the path
+    Path path;
+    auto v = v2;
+    while (v != v1){
+      path.steps.push_back(v);
+      if (prev.find(v) != prev.end()){
+        auto v0 = prev[v];
+        path.sumDistance += findConnection(v0,v);
+        v = v0;
+      }
+      else break;
+    }
+    return path;
   }
 };
 
