a8916d201284dadf47424611be5c603ceccfe2b1
starcolon <tao@starcolon.com>
2020-06-14 23:17:42 +0200

    017 add stacktrace handler

--- 017-kosaraju-cpp/src/Main.cpp
+++ 017-kosaraju-cpp/src/Main.cpp
@@ -3,11 +3,33 @@
 #include <sstream>
 #include <iterator>
 
+// Following imports are for stacktracing
+#include <execinfo.h>
+#include <signal.h>
+#include <stdlib.h>
+#include <unistd.h>
+
 #include "Data.h"
 
 using namespace std;
 
+// @href https://stackoverflow.com/questions/77005/how-to-automatically-generate-a-stacktrace-when-my-gcc-c-program-crashes
+inline void segFaultHandler(int sig) {
+  void *array[10];
+  size_t size;
+
+  // get void*'s for all entries on the stack
+  size = backtrace(array, 10);
+
+  // print out all the frames to stderr
+  fprintf(stderr, "Error: signal %d:\n", sig);
+  backtrace_symbols_fd(array, size, STDERR_FILENO);
+  exit(1);
+}
+
 int main(int argc, char** argv){
+
+  signal(SIGSEGV, segFaultHandler);
 
   cout << "Dijkstra ..." << endl;
 
@@ -60,6 +82,10 @@ int main(int argc, char** argv){
 
   cout << "Input complete " << endl;
   cout << g << endl;
+
+  // Verify if the graph is strongly connected
+  cout << "Graph strongly connected : " << g.isStronglyConnected() << endl;
+
   input = "";
 
   while (input != "n"){
@@ -84,5 +110,4 @@ int main(int argc, char** argv){
     }
   }
 }
-
 
