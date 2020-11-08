0db21a02a23a70ba699135595db9c3b089c5d7d8
Tao PR <pataoengineer@gmail.com>
2020-11-09 00:34:41 +0100

    018 wip expand reachability

--- 018-graph-optimisation/src/Analyse.cpp
+++ 018-graph-optimisation/src/Analyse.cpp
@@ -46,3 +46,19 @@ void topInbounds(Graph& g, int num){
   auto least = inbounds.top();
   cout << "Least inbound = " << get<0>(least) << " : only " << get<1>(least) << " inbounds" << endl;
 }
+
+void findReachability(Graph& g, string from, string to, int maxDegree){
+  cout << "----------------------" << endl;
+  cout << "Finding all reachabilities from : " <<
+    from << " -> " << to << endl;
+
+  vector<string> s {from};
+  Path p {.stops = s, .sumDistance = 0 };
+  vector<Path> paths {p};
+  paths = g.expandReach(to, maxDegree, paths);
+
+  for (auto& p : paths){
+    cout << p << endl;
+  }
+}
+
