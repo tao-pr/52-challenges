9d8f7ab3a7edf712e18b499cb390b5e5099426be
Tao PR <pataoengineer@gmail.com>
2020-11-09 22:36:42 +0100

    018 move distance func

--- 018-graph-optimisation/src/Main.cpp
+++ 018-graph-optimisation/src/Main.cpp
@@ -188,21 +188,6 @@ Graph readAirportsFromFile(string filename, Graph &g){
   return g;
 }
 
-double distance(tuple<double,double> p1, tuple<double,double> p2){
-  // Haversine
-  const double R = 6371e3;
-  double pp = M_PI / 180;
-  double phi1 = get<0>(p1) * pp;
-  double phi2 = get<0>(p2) * pp;
-  double deltaPhi = (get<0>(p2) - get<0>(p1)) * pp;
-  double deltaLambda = (get<1>(p2) - get<1>(p1)) * pp;
-  double a = sin(deltaPhi/2) * sin(deltaPhi/2) +
-    cos(phi1) * cos(phi2) *
-    sin(deltaLambda/2) * sin(deltaLambda/2);
-  double c = 2 * atan2(sqrt(a), sqrt(1-a));
-  return R * c * 0.001; // km
-}
-
 Graph updateRouteDistances(Graph& g){
   cout << "Updating edge distances ... " << endl;
   // Update route distances as edge cost
