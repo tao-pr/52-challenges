6982744d9c1865a49e600f1674d3ae0672bdc72b
starcolon <tao@starcolon.com>
2020-11-11 19:27:33 +0100

    019 reverse graph

--- 019-subgraph-optimisation/headers/Analyse.hpp
+++ 019-subgraph-optimisation/headers/Analyse.hpp
@@ -6,3 +6,4 @@ void analyse(Graph& g);
 void topOutbounds(Graph& g, int num);
 void topInbounds(Graph& g, int num);
 void findReachability(Graph& g, string from, string to, int maxDegree, double maxDistance);
+void analyseSubgraph(Graph&g, set<string> airports);
