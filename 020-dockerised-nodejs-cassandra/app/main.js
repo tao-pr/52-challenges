51dc6840cf073b6c87f747a9c054dd6ab380e6ba
starcolon <tao@starcolon.com>
2020-12-23 23:38:01 +0100

    020 handle exception

--- 020-dockerised-nodejs-cassandra/app/main.js
+++ 020-dockerised-nodejs-cassandra/app/main.js
@@ -5,18 +5,23 @@ const C = new cassandra.Client({
   keyspace: 'ks1'
 });
 
-const sql_create_ks = "CREATE KEYSPACE IF NOT EXISTS ks1 WITH replication \
-  = {'class':'SimpleStrategy', 'replication_factor':1};";
-
-const sql_create_tb = "CREATE TABLE IF NOT EXISTS ks1.tb1 (\
-  id bigint PRIMARY_KEY,\
-  ts timestamp,\
-  v text\
-  )";
-const query = 'SELECT name, email FROM users WHERE key = ?';
+const sql_prepare = 
+  "CREATE KEYSPACE IF NOT EXISTS ks1 WITH replication \
+    = {'class':'SimpleStrategy', 'replication_factor':1}; \
+  USE ks1;\
+  CREATE TABLE IF NOT EXISTS tb1 (\
+    id bigint PRIMARY_KEY,\
+    ts timestamp,\
+    v text); \
+  INSERT INTO tb1 \
+    (id, ts, v) \
+    VALUES (0, toTimestamp(toDate(now())), 'sample');\
+  ";
 
 // Upon startup, prepare the database
-C.execute(sql_create_ks)
+C.execute(sql_prepare)
   .then(_ => console.log("Cassandra keyspace created."))
-  .then(_ => C.execute(sql_create_tb))
-  .then(_ => console.log("Cassandra table created."))
+  .catch(e => console.error(e));
+
+
+// Start a simple REST server
