9063a1e4e3df49513917405f7d6552e69c3e2fd6
starcolon <tao@starcolon.com>
2020-12-24 01:10:54 +0100

    020 fix cql

--- 020-dockerised-nodejs-cassandra/app/main.js
+++ 020-dockerised-nodejs-cassandra/app/main.js
@@ -1,27 +1,38 @@
 const cassandra = require('cassandra-driver');
 const C = new cassandra.Client({
-  contactPoints: ['h1', 'h2'],
-  localDataCenter: 'datacenter1',
-  keyspace: 'ks1'
+  contactPoints: ['0.0.0.0:9042'],
+  localDataCenter: 'datacenter1'
 });
 
-const sql_prepare = 
-  "CREATE KEYSPACE IF NOT EXISTS ks1 WITH replication \
-    = {'class':'SimpleStrategy', 'replication_factor':1}; \
-  USE ks1;\
-  CREATE TABLE IF NOT EXISTS tb1 (\
-    id bigint PRIMARY_KEY,\
-    ts timestamp,\
-    v text); \
-  INSERT INTO tb1 \
-    (id, ts, v) \
-    VALUES (0, toTimestamp(toDate(now())), 'sample');\
-  ";
+/**
+ * Prepare cassandra
+ *
+ * API REF: https://docs.datastax.com/en/developer/nodejs-driver/4.6/api/class.Client/
+ */
+async function prep(){
+  try {
+    await C.connect();
+    await C.execute("CREATE KEYSPACE IF NOT EXISTS ks1 WITH replication \
+      = {'class':'SimpleStrategy', 'replication_factor':1}");
+    await C.execute("USE ks1");
+    await C.execute("CREATE TABLE IF NOT EXISTS tb1 (\
+      id bigint PRIMARY KEY,\
+      ts timestamp,\
+      v text)");
+    await C.execute("INSERT INTO tb1 \
+      (id, ts, v) \
+      VALUES (0, toTimestamp(toDate(now())), 'sample')");
+  } finally {
+    await C.shutdown();
+  }
+}
+
 
 // Upon startup, prepare the database
-C.execute(sql_prepare)
-  .then(_ => console.log("Cassandra keyspace created."))
-  .catch(e => console.error(e));
-
+prep();
 
 // Start a simple REST server
+// TAOTODO
+
+
+
