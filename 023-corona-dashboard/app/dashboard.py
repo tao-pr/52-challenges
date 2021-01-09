d1fe2eac9dc7578e0ea2e4f9972aa53287cec7f7
starcolon <tao@starcolon.com>
2021-01-10 00:11:31 +0100

    023 init panels

--- 023-corona-dashboard/app/dashboard.py
+++ 023-corona-dashboard/app/dashboard.py
@@ -3,7 +3,22 @@ import dash_html_components as html
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div()
+app.layout = html.Div(
+  children=[
+    html.Div(
+      className='row',
+      children=[
+        html.Div(
+          className='four columns div-user-controls bg-grey',
+          children=[
+            html.H2('COVID-19 tracking'),
+            html.P('foobar')
+          ]
+        ), 
+        html.Div(
+          className='two columns div-user-controls'),
+      ])
+  ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
