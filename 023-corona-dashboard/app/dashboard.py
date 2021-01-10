6fd197d8bb4e8e07439ae6ba1e1d225bd674c44c
Tao PR <pataoengineer@gmail.com>
2021-01-10 10:35:24 +0100

    023 add init style

--- 023-corona-dashboard/app/dashboard.py
+++ 023-corona-dashboard/app/dashboard.py
@@ -1,22 +1,27 @@
 import dash
 import dash_html_components as html
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__, external_stylesheets=['assets/style.css'])
 
 app.layout = html.Div(
   children=[
     html.Div(
-      className='row',
+      className='col',
+      style={'width': '100%', 'height': '100%'},
       children=[
         html.Div(
-          className='four columns div-user-controls bg-grey',
+          className='four columns div-user-controls',
           children=[
-            html.H2('COVID-19 tracking'),
+            html.H1('COVID-19 tracking'),
             html.P('foobar')
           ]
         ), 
         html.Div(
-          className='two columns div-user-controls'),
+          className='two columns div-user-controls',
+          children=[
+            html.P('aaaa')
+          ]
+        ),          
       ])
   ])
 
