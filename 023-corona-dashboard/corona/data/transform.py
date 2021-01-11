9bb99d09fec0fa14fd8d182379240e54d848a3fa
starcolon <tao@starcolon.com>
2021-01-12 00:32:31 +0100

    023 wip plotagg

--- 023-corona-dashboard/corona/data/transform.py
+++ 023-corona-dashboard/corona/data/transform.py
@@ -1,19 +1,30 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 
-def aggregate_over_time(df, period='d'):
+def aggregate_over_time(df, period='d', aggregator={'total_cases': 'sum'}):
   if period='d':
-    return df.groupby('date')
+    dfg = df.groupby('date')
   elif period='w':
     # Counting week as 7 days from day zero, not by calendar year
     dates = df.date.unique()
     df_dates = pd.DataFrame({n: np.arange(len(dates)), date: dates})
     df_dates.loc[:, 'wk'] = df_dates['n'] // 7
-    df = df.merge(df_dates[['date','wk']], on=['date'])
-    return df.groupby('wk')
+    dfg = df.merge(df_dates[['date','wk']], on=['date']).groupby('wk')
   elif period='m':
     strip_date_off = np.vectorize(lambda d: d[:7])
     df.loc[:, 'period'] = df['date'].apply(strip_date_off)
-    return df.groupby('period')
+    dfg = df.groupby('period')
   else:
     raise ValueError(f'Unsupported period : {period}')
+
+  dfg = dfg.agg(aggregator)
+  return dfg.reset_index()
+
+def plot_agg(df, country_list=['Germany'], aggregator={'total_cases': 'sum'}, period='d'):
+  dfg = aggregate_over_time(
+    df=df[df.location.isin(country_list)],
+    period=period,
+    aggregator=aggregator)
+  
+  # TAOTODO plot it
