c904eb5cd7bb347130958c0aa91bdd803bc4ab20
starcolon <tao@starcolon.com>
2021-01-23 17:08:40 +0100

    024 update opencv version

--- 024-table-reconstruction-from-image/make.sh
+++ 024-table-reconstruction-from-image/make.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export OPENCV_DIR=/usr/local/Cellar/opencv/4.1.0_2/
+export OPENCV_DIR=/usr/local/Cellar/opencv/4.5.0_5/
 export OPENCV_INCLUDE_DIR=$OPENCV_DIR/include/opencv4/
 export OPENCV_LIB_DIR=$OPENCV_DIR/lib/
 
