d3f7692ff911d9b0581561c07ee65d0ed58fd92c
starcolon <tao@starcolon.com>
2021-01-23 16:56:03 +0100

    024 init

---
+++ 024-table-reconstruction-from-image/make.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export OPENCV_DIR=/usr/local/Cellar/opencv/4.1.0_2/
+export OPENCV_INCLUDE_DIR=$OPENCV_DIR/include/opencv4/
+export OPENCV_LIB_DIR=$OPENCV_DIR/lib/
+
+rm -rf bin/*
+
+cd bin
+
+echo "================"
+echo "Preparing recipe"
+echo "================"
+cmake -DCMAKE_CXX_COMPILER=$(which g++) \
+      -DCMAKE_CXX_FLAGS="-std=c++11 -Wall -g -O1" -LAH ..
+
+echo "================"
+echo "Building..."
+echo "================"
+make
