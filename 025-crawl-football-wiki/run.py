2d6c6b83ec378f40a51da77ea3d793cace87d1d0
starcolon <tao@starcolon.com>
2021-02-03 00:07:53 +0100

    025 discard extra time

--- 025-crawl-football-wiki/run.py
+++ 025-crawl-football-wiki/run.py
@@ -1,5 +1,5 @@
 from crawler.lib import *
 
 if __name__ == '__main__':
-  matches = download_team_page('https://en.wikipedia.org/wiki/2014 –15_Real_Madrid_CF_season')
+  matches = download_team_page('https://en.wikipedia.org/wiki/2014–15_Real_Madrid_CF_season')
   print(matches)
