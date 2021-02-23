1ca6ef132dc47a89ce4de8a9253989301dbc59d6
starcolon <tao@starcolon.com>
2021-02-24 00:50:29 +0100

    026 eval random model

--- 026-ab-test-simulation/abtest/src/main/scala/tao/Main.scala
+++ 026-ab-test-simulation/abtest/src/main/scala/tao/Main.scala
@@ -17,13 +17,33 @@ case object CTRExp extends Experiment {
   }
 }
 
+case object CTRRandomExp extends Experiment {
+  override val variantA = GaussianRandomHypothesis(
+    mean=0.0, variance=0.00125, cap=Some(-0.25, 0.25)
+  )
+  override val variantB = GaussianRandomHypothesis(
+    mean=0.0, variance=0.013, cap=Some(-0.25, 0.25)
+  )
+  override def measureSample(samples: Seq[Double]): Double = {
+    // Just mean
+    samples.sum / samples.size.toDouble
+  }
+}
+
 
 
 object Main extends App {
   Console.println("------------------------------")
-  Console.println("Measuring CTR from A/B test")
+  Console.println("Measuring CTR improvement from A/B test")
   val ctrValue = CTRExp.evaluateValue(
     confidence=0.95,
     numBins=500)
-  Console.println(s"At confidence level = 95%, CTR rate = ${ctrValue.get}")
+  Console.println(s"At confidence level = 95%, % CTR rate increase = ${ctrValue.get*100} %")
+
+  Console.println("------------------------------")
+  Console.println("Measuring CTR improvement from A/B test (Random Model)")
+  val ctrRandomValue = CTRRandomExp.evaluateValue(
+    confidence=0.95,
+    numBins=500)
+  Console.println(s"At confidence level = 95%, % CTR rate increase = ${ctrRandomValue.get*100} %")
 }
