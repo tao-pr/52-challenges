109480d312c598f7b1d93fdea09cab36af13cf0c
starcolon <tao@starcolon.com>
2021-02-24 00:01:22 +0100

    026 generate bootstrap samples

--- 026-ab-test-simulation/abtest/src/main/scala/tao/Main.scala
+++ 026-ab-test-simulation/abtest/src/main/scala/tao/Main.scala
@@ -1,9 +1,26 @@
 package tao
 
+import scala.util.Random
+import tao.lib._
+
+// Target: Maximise % Increase of CTR
+case object CTRExp extends Experiment {
+  override val variantA = GaussianRandomHypothesis(
+    mean=0.0, variance=0.00125, cap=Some(-0.25, 0.25)
+  )
+  override val variantB = GaussianRandomHypothesis(
+    mean=1.45, variance=0.013, cap=Some(-0.01, 2.00)
+  )
+  override def measureSample(samples: Seq[Double]): Double = {
+    // Just mean
+    samples.sum / samples.size.toDouble
+  }
+}
+
+
+
 object Main extends App {
-  // Main entry
-
-  // Generate observation data
-
-  // ???
+  val sigCTR = CTRExp.evaluateSignificance(
+    confidence=0.95,
+    numBins=500)
 }
