2631538647e1358ea97cf0391261e69734f7593b
Tao PR <pataoengineer@gmail.com>
2021-02-24 22:51:12 +0100

    027 init

---
+++ 027-array-puzzles/puzzle/src/test/scala/puzzles/ArraySpec.scala
@@ -0,0 +1,14 @@
+package example
+
+import org.scalatest.funspec.AnyFunSpec
+import org.scalatest.matchers.should.Matchers
+
+class HelloSpec extends AnyFunSpec with Matchers {
+  
+  describe("Single array puzzles"){
+    it("Remove duplicates from array"){
+
+    }
+  }
+
+}
