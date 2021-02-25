8abd347731940ddc7175011867241b0f2e06c64e
Tao PR <pataoengineer@gmail.com>
2021-02-26 00:53:44 +0100

    028 wip trapped water

--- 028-puzzles-python/test_array.py
+++ 028-puzzles-python/test_array.py
@@ -48,3 +48,45 @@ def test_median_one_pass():
 
   assert median(ns) == 3
 
+
+def test_trap_rain_water():
+  # REF: https://leetcode.com/problems/trapping-rain-water/
+    
+  def eval(blocks):
+    sum_vol = 0
+    h = 0
+    i = 0
+    for b in blocks:
+      if b>h:
+        # Perhaps begin of left bound
+        h = b
+      elif b<h:
+        # Concave!
+        vol, next_blocks = project_to_right(h, blocks[i:])
+        if vol>0:
+          # concave has right boundary
+          # Break and start a new scan from next right bound
+          sum_vol += vol
+          sum_vol += eval(next_blocks)
+          return sum_vol
+        else:
+          # concave has no right boundary, startover
+          h = b
+      i += 1
+    return sum_vol
+
+  def project_to_right(h, blocks):
+    vol = 0
+    has_end = False
+    i = 0
+    for b in blocks:
+      if b>=h:
+        # Find right boundary
+        return vol, blocks[i+1:]
+      else:
+        vol += h-b
+    return 0, [] # No right boundary
+
+
+  assert eval([0,1,0,2,1,0,1,3,2,1,2,1]) == 6
+  assert eval([4,2,0,3,2,5]) == 9
