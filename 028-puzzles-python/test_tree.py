d8dabcb1733f3b87f87892fd545b57f22fe1b85e
Tao PR <pataoengineer@gmail.com>
2021-02-28 13:47:33 +0100

    028 flatten tree

--- 028-puzzles-python/test_tree.py
+++ 028-puzzles-python/test_tree.py
@@ -42,3 +42,35 @@ def test_count_visible__left_nodes():
   root_2.right.right = TreeNode(16)
   assert visible_nodes(root_2) == 5
 
+
+def test_flatten_tree_to_list():
+  # REF: https://leetcode.com/problems/flatten-binary-tree-to-linked-list/
+  def flatten(tree):
+    vec = [tree.val]
+    if tree.left is not None:
+      vec = flatten(tree.left) + vec
+    if tree.right is not None:
+      vec = vec + flatten(tree.right)
+    return vec
+
+
+
+  # Test
+  #             8
+  #           /   \
+  #          3     10
+  #         / \      \    
+  #        1   6     14
+  #           / \    /
+  #          4   7  13
+  root_1 = TreeNode(8)
+  root_1.left = TreeNode(3)
+  root_1.right = TreeNode(10)
+  root_1.left.left = TreeNode(1)
+  root_1.left.right = TreeNode(6)
+  root_1.left.right.left = TreeNode(4)
+  root_1.left.right.right = TreeNode(7)
+  root_1.right.right = TreeNode(14)
+  root_1.right.right.left = TreeNode(13)
+  assert flatten(root_1) == [1,3,4,6,7,8,10,13,14]
+
