01065a2ec6d1151e2aaa0c431096d4c7c00456a4
starcolon <tao@starcolon.com>
2021-02-28 21:20:51 +0100

    028 str to num

--- 028-puzzles-python/test_string.py
+++ 028-puzzles-python/test_string.py
@@ -24,3 +24,40 @@ def test_rotational_cipher():
   assert encode_letter('Z', 'A', 'Z', 3) == 'C'
   assert rotationalCipher("Zebra-493?", 3) == "Cheud-726?"
   assert rotationalCipher("abcdefghijklmNOPQRSTUVWXYZ0123456789", 39) == "nopqrstuvwxyzABCDEFGHIJKLM9012345678"
+
+
+def test_string_to_number():
+  def str_to_num(q):
+    tokens = q.split(' ')
+    return parse(tokens)
+
+  def parse(tokens, prev=None):
+    if len(tokens)==0:
+      return 0
+    tk = tokens[-1]
+    digits = ['zero','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirten']
+    dec = ['zero','ten','twenty','thirty','fourty','fifty','sixty','seventy','eighty','ninty']
+    
+    n = 1
+    if prev == 'hundred':
+      n = 100
+
+    if tk in digits:
+      return digits.index(tk)*n + parse(tokens[:-1])
+    elif tk == 'twenty':
+      return 20*n + parse(tokens[:-1])
+    elif tk == 'fifteen':
+      return 15*n + parse(tokens[:-1])
+    elif 'teen' in tk:
+      return digits.index(tk.replace('teen',''))*n + parse(tokens[:-1])
+    elif tk in dec:
+      return dec.index(tk)*n*10 + parse(tokens[:-1])
+    elif 'hundred' == tk:
+      return parse(tokens[:-1], 'hundred')
+    else:
+      return 0
+
+  assert str_to_num("fourty four") == 44
+  assert str_to_num("two hundred eleven") == 211
+  assert str_to_num("seven hundred thirty five") == 735
+  assert str_to_num("five hundred fifteen") == 515
