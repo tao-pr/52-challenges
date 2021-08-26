08c60918f2d6535feb0d6dc5265ed2bafe065d28
Tao PR <pataoengineer@gmail.com>
2021-08-26 11:22:43 +0200

    030 - wip

--- 030-read-structure-of-pdf/pdf/lib.py
+++ 030-read-structure-of-pdf/pdf/lib.py
@@ -8,7 +8,7 @@ from lxml import etree
 BLOCK_IMAGE = 1
 BLOCK_TEXT = 0
 
-Block = namedtuple("Block", ['tl','br','w','h','content', 'type'])
+Block = namedtuple("Block", ['tl','br','w','h','content', 'type', 'density'])
 
 def read_pdf2(path: str):
   """
@@ -32,24 +32,19 @@ def read_pdf(path: str):
   pages = [pdf.load_page(n) for n in range(num_pages)]
   
   # Read as textblocks
+  only_text = lambda b: b.type == BLOCK_TEXT
   ptextblocks = map(lambda x: \
-    map(parse_text_block, x.get_text_blocks()), pages) # pages -> blocks
+    filter(only_text, map(parse_text_block, x.get_text_blocks())), 
+    pages) # pages -> blocks
 
   # Read as HTML
   phtmls = map(lambda p: parse_html(p.get_textpage().extractHTML()), pages) # pages -> html
 
+  # Create content tree
+  content = gen_content_tree(toc, ptextblocks)
 
-  """
-  [...
-    (28.346399307250977,
-    72.4287109375,
-    555.9425659179688,
-    91.57421875,
-    'Allgemeine Informationen zu Produktbezeichnungen gemäß § 15  Zahlungskontengesetz\nSoweit im Folgenden die Begrifflichkeiten „Commerzbank Girocard“, „Mastercard Debit“ oder „Virtual Debit Card“ genannt werden, entsprechen diese der standar disierten Zahlungskontenterminologie „Ausgabe einer Debitkarte“.\nSoweit im Folgenden die Begrifflichkeiten „PremiumKreditkarte“, „Young Visa Kreditkarte“, „ClassicKreditkarte“, „GoldKreditkarte“, „Prepaid Karte“ oder „Prepaid Karte Junior“ genannt werden, entsprechen diese der standardisierten Zahlungskontenterminologie „Ausgabe einer Kreditkarte“.\n',
-    138,
-    0)]
-  """
-  p = next(ptextblocks)
+
+  p = list(next(ptextblocks))
 
   import IPython
   IPython.embed()
@@ -61,9 +56,20 @@ def parse_text_block(bl):
   # NOTE: dimension of textblock may not accurately infer how big or small
   # the whole text is. The whole block may be rotated and its bounding rect 
   # will get bigger than expected.
-  b = Block(tl=(x0,y0), br=(x1,y1), w=w, h=h, content=obj, type=bl_type)
+
+  if bl_type == BLOCK_TEXT:
+    density = (w*h) / len(obj)
+  else:
+    density = None
+
+  b = Block(tl=(x0,y0), br=(x1,y1), w=w, h=h, content=obj, type=bl_type, density=density)
   return b
 
 def parse_html(ht):
   tree = etree.parse(StringIO(ht))
   return tree
+
+def gen_content_tree(toc, blocks):
+  # TAOTODO
+
+  pass
