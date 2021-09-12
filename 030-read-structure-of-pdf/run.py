8a1196fd1dfd2ed1522f4aeb9709973f0415803f
starcolon <tao@starcolon.com>
2021-09-12 23:23:25 +0200

    030 extract to csv

--- 030-read-structure-of-pdf/run.py
+++ 030-read-structure-of-pdf/run.py
@@ -2,11 +2,13 @@ import os
 import IPython
 import glob
 
-from pdf.lib import read_pdf
+from pdf.lib import read_pdf, save_to
 
 if __name__=="__main__":
+  # Read PDF and convert to tabular data
+  output = 'data'
   ls = glob.glob(os.path.expanduser('~/data/sustain/') + r'*.pdf')
-  for f in ls:
+  for i,f in enumerate(ls):
     ctree = read_pdf(f)
-    IPython.embed()
+    save_to(ctree, output + str(i) + '.csv')
     break
