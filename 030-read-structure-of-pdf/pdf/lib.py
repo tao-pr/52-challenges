37072f1e4a7ba3b0a926cca028a5c1fe63740604
Pathompong Ruangyam <tao.ruangyam@ing.de>
2021-09-13 14:55:35 +0200

    030 format output

--- 030-read-structure-of-pdf/pdf/lib.py
+++ 030-read-structure-of-pdf/pdf/lib.py
@@ -1,3 +1,4 @@
+import os
 from collections import namedtuple
 from functools import partial
 from io import StringIO
@@ -8,14 +9,17 @@ from lxml import etree
 BLOCK_IMAGE = 1
 BLOCK_TEXT = 0
 
-Block = namedtuple("Block", ['tl','br','w','h','content', 'type', 'density'])
-SBlock = namedtuple("SBlock", ['page','content','priority'])
+Block = namedtuple("Block", ['filename','tl','br','w','h','content', 'type', 'density'])
+SBlock = namedtuple("SBlock", ['filename','page','content','priority'])
+
+def escape(t):
+  return t.replace("\"", "").strip()
 
 def save_to(sbs, path):
   with open(path, 'w') as f:
     f.write(f'page,priority,content\n')
     for s in sbs:
-      f.write(f'{s.page},{s.priority},{s.content}\n')
+      f.write(f'{s.page},{s.priority},"{escape(s.content)}"\n')
 
 def read_pdf2(path: str):
   """
@@ -40,9 +44,10 @@ def read_pdf(path: str, verbose: bool=False):
   pages = [pdf.load_page(n) for n in range(num_pages)]
   
   # Read as textblocks
+  filename = os.path.basename(path)
   only_text = lambda b: b.type == BLOCK_TEXT
   ptextblocks = map(lambda x: \
-    filter(only_text, map(parse_text_block, x.get_text_blocks())), 
+    filter(only_text, map(parse_text_block(filename), x.get_text_blocks())), 
     pages) # pages -> blocks
 
   # Create content tree
@@ -50,21 +55,23 @@ def read_pdf(path: str, verbose: bool=False):
 
   return ctree
 
-def parse_text_block(bl):
-  x0, y0, x1, y1, obj, bl_no, bl_type = bl
-  w = x1-x0
-  h = y1-y0
-  # NOTE: dimension of textblock may not accurately infer how big or small
-  # the whole text is. The whole block may be rotated and its bounding rect 
-  # will get bigger than expected.
+def parse_text_block(filename):
+  def internal(bl):
+    x0, y0, x1, y1, obj, bl_no, bl_type = bl
+    w = x1-x0
+    h = y1-y0
+    # NOTE: dimension of textblock may not accurately infer how big or small
+    # the whole text is. The whole block may be rotated and its bounding rect 
+    # will get bigger than expected.
 
-  if bl_type == BLOCK_TEXT:
-    density = (w*h) / len(obj)
-  else:
-    density = None
+    if bl_type == BLOCK_TEXT:
+      density = (w*h) / len(obj)
+    else:
+      density = None
 
-  b = Block(tl=(x0,y0), br=(x1,y1), w=w, h=h, content=obj, type=bl_type, density=density)
-  return b
+    b = Block(filename=filename, tl=(x0,y0), br=(x1,y1), w=w, h=h, content=obj, type=bl_type, density=density)
+    return b
+  return internal
 
 def parse_html(ht):
   tree = etree.parse(StringIO(ht))
@@ -78,5 +85,5 @@ def gen_content_tree(blocks):
       if len(block.content.strip())<=3:
         continue
       pr = block.density
-      chain.append(SBlock(page=p, content=block.content.replace('\n','').strip(), priority=pr))
+      chain.append(SBlock(filename=block.filename, page=p, content=block.content.replace('\n','').strip(), priority=pr))
   return chain
--- 030-read-structure-of-pdf/run.py
+++ 030-read-structure-of-pdf/run.py
@@ -6,9 +6,10 @@ from pdf.lib import read_pdf, save_to
 
 if __name__=="__main__":
   # Read PDF and convert to tabular data
-  output = 'data'
+  output = 'out'
   ls = glob.glob(os.path.expanduser('~/data/sustain/') + r'*.pdf')
   for i,f in enumerate(ls):
+    print(f'({i}) processing : {f}')
     ctree = read_pdf(f)
-    save_to(ctree, output + str(i) + '.csv')
+    save_to(ctree, output + '/' + ctree[0].filename.split('.')[0] + '.csv')
     break
