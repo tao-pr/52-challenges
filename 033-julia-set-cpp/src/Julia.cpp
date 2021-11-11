27e14c6c902c6d153f8fe73acc3d4f67d4915ccb
Tao PR <pataoengineer@gmail.com>
2021-11-11 14:29:30 +0100

    033 init code

---
+++ 033-julia-set-cpp/src/Julia.cpp
@@ -0,0 +1,18 @@
+#include "Julia.hpp"
+
+
+int JuliaSet::convergence(Complex<double>& z, Complex<double>& c, int nIter) const
+{
+
+}
+
+JuliaSet::JuliaSet(int nMaxIters, double bound)
+{
+
+}
+
+void JuliaSet::render(double reMin, double reMax, double imMin, double imMax, double resolution) const
+{
+
+}
+
