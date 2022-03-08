70187a18c48d587ab23b129219a79dfcb34b061c
Tao PR <pataoengineer@gmail.com>
2022-03-08 21:46:43 +0100

    034 schedulers work

--- 034-akka-singleton/src/main/scala/me/primecoder/Actors.scala
+++ 034-akka-singleton/src/main/scala/me/primecoder/Actors.scala
@@ -1,32 +1,45 @@
 package me.primecoder
 
-import akka.actor.typed.ActorRef
-import akka.actor.typed.ActorSystem
-import akka.actor.typed.Behavior
-import akka.actor.typed.scaladsl.Behaviors
+import akka.actor.ActorRef
+import akka.actor.ActorSystem
 import akka.actor.Actor
+import akka.event.Logging
 
 import scala.collection.mutable.HashMap
 
-class MutableSingleton extends Actor {
+class ScheduledActor extends Actor with akka.actor.ActorLogging {
   
   def receive: Actor.Receive = {
     case AddKeySignal =>
-      val key = MutableSingleton.data.size+1
+      val key = MutableSingleton.size+1
       val value = scala.util.Random.nextLong.toString
-      Console.println(s"Singleton receiving update signal : $key")
-      Console.println(s"... Updating $key -> $value")
-      MutableSingleton.data.addOne(key, value)
+      log.info(s"Singleton receiving update signal : $key")
+      log.info(s"... Updating $key -> $value")
+      MutableSingleton.add(key, value)
 
     case GetKeySignal(key) =>
-      Console.println(s"Singleton receiving get signal : $key")
-      Console.println(s"... Obtaining $key -> ${MutableSingleton.data.get(key)}")
+      log.info(s"Singleton receiving get signal : $key")
+      log.info(s"... Obtaining $key -> ${MutableSingleton.get(key)}")
   }
 
 }
 
-object MutableSingleton extends MutableSingleton {
+object MutableSingleton {
   private var data = new HashMap[Int, String]
+
+  def add(key: Int, value: String) = {
+    data.addOne(key, value)
+  }
+
+  def get(key: Int) = {
+    data.get(key)
+  }
+
+  def replace(m: HashMap[Int, String]) = {
+    data = m
+  }
+
+  def size = data.size
 }
 
 
--- 034-akka-singleton/src/main/scala/me/primecoder/AkkaMain.scala
+++ 034-akka-singleton/src/main/scala/me/primecoder/AkkaMain.scala
@@ -6,20 +6,19 @@ import akka.actor.ActorRef
 import akka.actor.ActorSystem
 import akka.actor.Props
 import akka.actor.Cancellable
+import akka.event.Logging
 
 import com.typesafe.config.ConfigFactory
 import scala.concurrent.ExecutionContextExecutor
 import scala.concurrent.duration._
 
-object MainRun extends App {  
+object MainRun extends App {
   lazy val appConfig = ConfigFactory.load()
-  implicit val actorSystem = ActorSystem("MainActor")
+  implicit lazy val actorSystem = ActorSystem("MainActor")
   implicit lazy val ec: ExecutionContextExecutor = actorSystem.dispatcher
 
-  Console.println("Starting app ...")
-
   // Start scheduler
-  val tick = actorSystem.actorOf(Props(classOf[MutableSingleton], this))
+  val tick = actorSystem.actorOf(Props(classOf[ScheduledActor]))
   val scheduler: Cancellable = actorSystem.scheduler.scheduleWithFixedDelay(
     5.seconds, // delay of first run
     10.seconds, // frequency
