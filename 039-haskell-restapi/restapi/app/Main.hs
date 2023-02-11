a01c05e87b304395f759bbbe21258bd073f27f72
Tao PR <pataoengineer@gmail.com>
2023-02-11 23:25:43 +0100

    039 zip and unzip work

--- 039-haskell-restapi/restapi/app/Main.hs
+++ 039-haskell-restapi/restapi/app/Main.hs
@@ -4,9 +4,12 @@ module Main (main) where
 
 import Lib
 import Payload
+import Data.Text (Text)
+import Data.Text.Lazy (fromStrict)
 import Control.Monad.IO.Class
 import Web.Scotty -- Scotty is a web framework inspired by Ruby's Sinatra
 import GHC.Base
+import Debug.Trace
 
 main :: IO ()
 main = routes
@@ -14,12 +17,28 @@ main = routes
 routes :: IO () -- taotodo: add DB connection as 1st arg
 routes = 
   scotty 3333 $ do
-    post "/validate" $
-      do
-        -- parse request body
-        payload <- jsonData
-        isValid <- liftIO $ returnIO $ isValidPayload payload
+    -- validate if [[Payload]] is valid
+    post "/validate" $ do
+      payload <- jsonData
+      traceM ("payload: " ++ show payload)
+      valid <- liftIO . returnIO $ isValid payload
+      json $ response valid
 
-        -- return
-        json $ valid "payload is valid"
+    -- extract payload containing gzipped of [[Unzipped]]
+    post "/extract" $ do
+      payload <- jsonData
+      traceM ("payload: " ++ show payload)
+      uz <- liftIO . returnIO $ unzipped payload
+      json $ uz
+
+    -- zip and return a utf8 bytestring of [[Unzipped]]
+    post "/zip" $ do 
+      uz <- jsonData -- for zip endpoint, data has to be "Unzipped" format
+      traceM ("received: " ++ show uz)
+      zz <- liftIO . returnIO $ zipMe uz
+      traceM ("zipped: " ++ show zz)
+      html $ fromStrict zz
+
+    get "/" $ do
+      html $ mconcat ["<bold>Hello, there!</bold>", "<p>", "this works!", "</p>"]
 
--- 039-haskell-restapi/restapi/src/Payload.hs
+++ 039-haskell-restapi/restapi/src/Payload.hs
@@ -1,12 +1,19 @@
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-module Payload (Payload, Unzipped, isValidPayload, unzipped, valid) where
+module Payload (Payload, response, Unzipped, isValid, unzipped, zipMe) where
 
+import Prelude hiding(null)
 import Data.IORef
-import Data.Text (Text)
-import Data.ByteString.UTF8 (ByteString)
+import Data.Text (Text, null, pack, unpack, splitOn)
+import Data.Text.Encoding
+import Data.Char (ord, chr)
 import Data.Aeson
+import Data.ByteString.Lazy (fromStrict, toStrict, ByteString)
+import Data.ByteString.Lazy.Internal (unpackChars, packChars)
+import Codec.Compression.GZip(decompress, compress)
 import GHC.Generics
+import Debug.Trace
 
 -- Raw request payload (with zipped data)
 data Payload = Payload 
@@ -21,6 +28,7 @@ data Unzipped = Null | Unzipped
     date :: Text,
     amount :: Int
   }
+  deriving (Generic, Show)
 
 data Response = Response
   { isError :: Bool,
@@ -29,15 +37,43 @@ data Response = Response
   deriving (Generic, Show)
 
 
+-- serialisables
 instance FromJSON Payload
 instance ToJSON Response
+instance ToJSON Unzipped
+instance FromJSON Unzipped
 
 
-isValidPayload :: Payload -> Bool
-isValidPayload p = error "not implemented"
+isValid :: Payload -> Bool
+isValid p = not . null $ sessionId p
 
-unzipped :: Payload -> Unzipped
-unzipped p = error "not implemented"
+response :: Bool -> Response
+response v = if v 
+  then Response False "valid request"
+  else Response True "invalid request"
 
-valid :: Text -> Response 
-valid t = Response True t
+-- decode digit from text
+t2c :: Text -> Char
+t2c t = chr (read (unpack t) :: Int)
+
+-- decode \x31\x139\x8\  ... into ByteString
+-- needs to do 'tail' since it splits into [,31,139,8,..]
+decodex :: Text -> ByteString
+decodex s = packChars . (map t2c) . tail $ splitOn "\\x" s
+
+unzipped :: Payload -> Maybe Unzipped
+unzipped p = 
+  -- needs `fromStrict` to convert ByteString -> Lazy.ByteString
+  let z = decompress . decodex . zipped $ p
+    in decode z :: Maybe Unzipped
+
+pref :: String -> String
+pref a = "\\x" ++ a
+
+zipMe :: Unzipped -> Text
+zipMe uz = 
+  let bytes = unpackChars . compress . encode $ uz
+    in pack $ concat $ map (pref . show . ord) $ bytes
+  
+
+
