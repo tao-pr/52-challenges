c258e210d1bcc6e85410db45d9964cb821153c85
Tao PR <pataoengineer@gmail.com>
2023-03-18 22:44:00 +0100

    040 sample

--- 040-monte-carlo-haskell/monte/src/Sample.hs
+++ 040-monte-carlo-haskell/monte/src/Sample.hs
@@ -2,13 +2,18 @@ module Sample where
 
 import Control.Parallel.Strategies
 import GHC.Base(returnIO)
+import System.Random
 
 data Point = Point Float Float 
   deriving Show
 
+
 genPar :: Int -> [IO Point]
-genPar num = [] -- taotodo
+genPar num = map gen [1..num]
 
-gen :: IO Point
-gen = returnIO $ Point 0 0 -- taotodo
-
+gen :: Int -> IO Point
+gen _ = do 
+    a <- getStdRandom (randomR (0, 1)) :: IO Float
+    b <- getStdRandom (randomR (0, 1)) :: IO Float
+    let p = Point a b
+    return p
